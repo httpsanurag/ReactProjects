8bae319df275ea192ef017bc239aa84d136b6233
Anurag Yadav <yadavanurag9528@gmail.com>
2025-03-24 19:23:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./global.css";
-createRoot(document.getElementById("root")).render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './global.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
